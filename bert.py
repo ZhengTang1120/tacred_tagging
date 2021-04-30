33d6c84c63782510b431ad8824505ab5081f2117
Zheng Tang <oscartang1120@gmail.com>
2021-04-29 17:01:08 -0700

    bert only

--- bert.py
+++ bert.py
@@ -11,69 +11,28 @@ class BERTencoder(nn.Module):
     def __init__(self):
         super().__init__()
         in_dim = 1024
-        self.model = BertModel.from_pretrained("mrm8488/spanbert-large-finetuned-tacred")
+        self.model = BertModel.from_pretrained("SpanBERT/spanbert-large-cased")
         self.classifier = nn.Linear(in_dim, 1)
-        self.pos_emb = nn.Embedding(6, 5, padding_idx=constant.PAD_ID)
 
     def forward(self, inputs):
-        words, masks, pos, ner, deprel, ent_pos, subj_pos, obj_pos, subj_type, obj_type = inputs
-        outputs = self.model(words)
+        words, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs
+        outputs = self.model(words, output_attentions=True)
         h = outputs.last_hidden_state
-        h = torch.cat([h, self.pos_emb(ent_pos)], dim=2)
         out = torch.sigmoid(self.classifier(outputs.pooler_output))
-
-        return h, out
+        
+        return outputs, out
 
 class BERTclassifier(nn.Module):
     def __init__(self, opt):
         super().__init__()
-        in_dim = 1029
+        in_dim = 1024
         self.classifier = nn.Linear(in_dim, opt['num_class'])
         self.opt = opt
 
-    def forward(self, h, masks, subj_pos, obj_pos):
-        subj_mask, obj_mask = subj_pos.eq(1000).unsqueeze(2), obj_pos.eq(1000).unsqueeze(2)
-        
-        pool_type = self.opt['pooling']
-        out_mask = masks.unsqueeze(2).eq(0) + subj_mask + obj_mask
-        cls_out = pool(h, out_mask.eq(0), type=pool_type)
+    def forward(self, h):
+        cls_out = h#pool(h, out_mask.eq(0), type=pool_type)
         logits = self.classifier(cls_out)
         return logits
-
-class Tagger(nn.Module):
-    def __init__(self):
-        super().__init__()
-        in_dim = 1029
-
-        self.tagger = nn.Linear(in_dim, 1)
-        self.threshold1 = 0.8
-        self.threshold2 = 0.2
-
-    def forward(self, h):
-
-        tag_logits = torch.sigmoid(self.tagger(h))
-        
-        return tag_logits
-
-    def generate_cand_tags(self, tag_logits, device):
-        cand_tags = [[]]
-        for t in tag_logits:
-            if t < self.threshold1 and t > self.threshold2:
-                temp = []
-                for ct in cand_tags:
-                    temp.append(ct+[0])
-                    ct.append(1)
-                cand_tags += temp
-                if len(cand_tags) > 2048:
-                    return None, -1
-            elif t > self.threshold1:
-                for ct in cand_tags:
-                    ct.append(1)
-            else:
-                for ct in cand_tags:
-                    ct.append(0)
-        with torch.cuda.device(device):
-            return torch.BoolTensor(cand_tags).cuda(), len(cand_tags)
 
 def pool(h, mask, type='max'):
     if type == 'max':
--- trainer.py
+++ trainer.py
@@ -8,7 +8,8 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
-from bert import BERTencoder, BERTclassifier, Tagger
+from model.bert import BERTencoder, BERTclassifier
+from model.decoder import Decoder
 from utils import constant, torch_utils
 
 from transformers import AdamW
@@ -34,16 +35,12 @@ class Trainer(object):
             exit()
         self.classifier.load_state_dict(checkpoint['classifier'])
         self.encoder.load_state_dict(checkpoint['encoder'])
-        self.tagger.load_state_dict(checkpoint['tagger'])
-        device = self.opt['device']
         self.opt = checkpoint['config']
-        self.opt['device'] = device
 
     def save(self, filename, epoch):
         params = {
                 'classifier': self.classifier.state_dict(),
                 'encoder': self.encoder.state_dict(),
-                'tagger': self.tagger.state_dict(),
                 'config': self.opt,
                 }
         try:
@@ -53,24 +50,16 @@ class Trainer(object):
             print("[Warning: Saving failed... continuing anyway.]")
 
 
-def unpack_batch(batch, cuda, device):
+def unpack_batch(batch, cuda):
     rules = None
     if cuda:
-        with torch.cuda.device(device):
-            inputs = [batch[0].to('cuda')] + [Variable(b.cuda()) for b in batch[1:10]]
-            labels = Variable(batch[10].cuda())
-            rules  = Variable(batch[12]).cuda()
+        inputs = [batch[0].to('cuda')] + [Variable(b.cuda()) for b in batch[1:9]]
+        labels = Variable(batch[9].cuda())
     else:
-        inputs = [Variable(b) for b in batch[:10]]
-        labels = Variable(batch[10])
-        rules  = Variable(batch[12])
+        inputs = [batch[0]] + [Variable(b) for b in batch[1:9]]
+        labels = Variable(batch[9])
     tokens = batch[0]
-    head = batch[5]
-    subj_pos = batch[6]
-    obj_pos = batch[7]
-    tagged = batch[-1]
-    lens = batch[5].eq(4).long().sum(1).squeeze()
-    return inputs, labels, rules, tokens, head, subj_pos, obj_pos, lens, tagged
+    return inputs, labels, tokens
 
 class BERTtrainer(Trainer):
     def __init__(self, opt, emb_matrix=None):
@@ -78,59 +67,31 @@ class BERTtrainer(Trainer):
         self.emb_matrix = emb_matrix
         self.encoder = BERTencoder()
         self.classifier = BERTclassifier(opt)
-        self.tagger = Tagger()
-        self.criterion = nn.CrossEntropyLoss(ignore_index=0)
-        self.criterion2 = nn.BCELoss()
-        self.parameters = [p for p in self.classifier.parameters() if p.requires_grad] + [p for p in self.encoder.parameters() if p.requires_grad]+ [p for p in self.tagger.parameters() if p.requires_grad]
+        self.criterion = nn.CrossEntropyLoss()
+        self.parameters = [p for p in self.classifier.parameters() if p.requires_grad] + [p for p in self.encoder.parameters() if p.requires_grad]
         if opt['cuda']:
-            with torch.cuda.device(self.opt['device']):
-                self.encoder.cuda()
-                self.tagger.cuda()
-                self.classifier.cuda()
-                self.criterion.cuda()
+            self.encoder.cuda()
+            self.classifier.cuda()
+            self.criterion.cuda()
+        #self.optimizer = torch_utils.get_optimizer(opt['optim'], self.parameters, opt['lr'])
         self.optimizer = AdamW(
             self.parameters,
             lr=opt['lr'],
         )
     
     def update(self, batch, epoch):
-        inputs, labels, rules, tokens, head, subj_pos, obj_pos, lens, tagged = unpack_batch(batch, self.opt['cuda'], self.opt['device'])
+        inputs, labels, tokens = unpack_batch(batch, self.opt['cuda'])
 
         # step forward
         self.encoder.train()
         self.classifier.train()
-        self.tagger.train()
         self.optimizer.zero_grad()
 
         loss = 0
-        h, b_out = self.encoder(inputs)
-        tagging_output = self.tagger(h)
-        loss = self.criterion2(b_out, (~(labels.eq(0))).to(torch.float32).unsqueeze(1))
-        if epoch <= 5:
-            for i, f in enumerate(tagged):
-                if f:
-                    loss += self.criterion2(tagging_output[i], rules[i].unsqueeze(1).to(torch.float32))
-                    logits = self.classifier(h[i], inputs[1][i].unsqueeze(0), inputs[6][i].unsqueeze(0), inputs[7][i].unsqueeze(0))
-                    loss += self.criterion(logits, labels.unsqueeze(1)[i])
-        else:
-            for i, f in enumerate(tagged):
-                if f:
-                    if loss == 0:
-                        loss = self.criterion2(tagging_output[i], rules[i].unsqueeze(1).to(torch.float32))
-                    else:
-                        loss += self.criterion2(tagging_output[i], rules[i].unsqueeze(1).to(torch.float32))
-                    logits = self.classifier(h[i], inputs[1][i].unsqueeze(0), inputs[6][i].unsqueeze(0), inputs[7][i].unsqueeze(0))
-                    loss += self.criterion(logits, labels.unsqueeze(1)[i])
-                elif labels[i] != 0:
-                    tag_cands, n = self.tagger.generate_cand_tags(tagging_output[i], self.opt['device'])
-                    if n != -1:
-                        logits = self.classifier(h[i], tag_cands, torch.cat(n*[inputs[6][i].unsqueeze(0)], dim=0), torch.cat(n*[inputs[7][i].unsqueeze(0)], dim=0))
-                        best = np.argmax(logits.data.cpu().numpy(), axis=0).tolist()[labels[i]]
-                        if loss == 0:
-                            loss = self.criterion2(tagging_output[i], tag_cands[best].unsqueeze(1).to(torch.float32))
-                        else:
-                            loss += self.criterion2(tagging_output[i], tag_cands[best].unsqueeze(1).to(torch.float32))
-                        loss += self.criterion(logits[best].unsqueeze(0), labels.unsqueeze(1)[i])
+        o, b_out = self.encoder(inputs)
+        h = o.pooler_output
+        logits = self.classifier(h)
+        loss += self.criterion(logits, labels)
         if loss != 0:
             loss_val = loss.item()
             # backward
@@ -141,33 +102,20 @@ class BERTtrainer(Trainer):
         return loss_val
 
     def predict(self, batch, id2label, tokenizer, unsort=True):
-        inputs, labels, rules, tokens, head, subj_pos, obj_pos, lens, tagged = unpack_batch(batch, self.opt['cuda'], self.opt['device'])
-        rules = rules.data.cpu().numpy().tolist()
+        inputs, labels, tokens = unpack_batch(batch, self.opt['cuda'])
         tokens = tokens.data.cpu().numpy().tolist()
-        orig_idx = batch[11]
+        orig_idx = batch[10]
         # forward
         self.encoder.eval()
         self.classifier.eval()
-        self.tagger.eval()
-        h, b_out = self.encoder(inputs)
-        tagging_output = self.tagger(h)
-        tagging_mask = torch.round(tagging_output).squeeze(2).eq(0)
-        tagging = torch.round(tagging_output).squeeze(2)
-        logits = self.classifier(h, tagging_mask, inputs[6], inputs[7])
+        o, b_out = self.encoder(inputs)
+        h = o.pooler_output
+        logits = self.classifier(h)
         loss = self.criterion(logits, labels)
-        probs = F.softmax(logits, 1) * torch.round(b_out)
+        probs = F.softmax(logits, 1)
         predictions = np.argmax(probs.data.cpu().numpy(), axis=1).tolist()
-        tags = []
-        for i, p in enumerate(predictions):
-            if p != 0:
-                t = tagging[i]
-                chunk = inputs[5].eq(4).long()[i].data.cpu().numpy().tolist()
-                t = t.data.cpu().numpy().tolist()
-                l = lens.data.cpu().numpy().tolist()[i]
-                tags += [t]
-            else:
-                tags += [[]]
+        tags = predictions
         if unsort:
-            _, predictions, probs, tags, rules, tokens = [list(t) for t in zip(*sorted(zip(orig_idx,\
-                    predictions, probs, tags, rules, tokens)))]
-        return predictions, tags, rules, tokens
+            _, predictions, probs = [list(t) for t in zip(*sorted(zip(orig_idx,\
+                    predictions, probs)))]
+        return predictions
