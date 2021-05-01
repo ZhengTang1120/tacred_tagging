0c09565132e942fff3a1d3ccc5b6abd14f9b4010
Zheng Tang <oscartang1120@gmail.com>
2021-05-01 03:52:57 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -70,26 +70,12 @@ other = 0
 for c, b in enumerate(batch):
     preds = trainer.predict(b, id2label, tokenizer)
     predictions += preds
-    tags += ts
-    goldt += tagged
     batch_size = len(preds)
     for i in range(batch_size):
         inputs += [[tokenizer.convert_ids_to_tokens(j) for j in ids[i]]]
 output = list()
 for i, p in enumerate(predictions):
         predictions[i] = id2label[p]
-        output.append({'gold_label':batch.gold()[i], 'predicted_label':id2label[p], 'raw_words':batch.words[i], 'predicted_tags':[], 'gold_tags':[]})
-        if p!=0:
-            if sum(goldt[i])!=0:
-                output[-1]['gold_tags'] = [goldt[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-                # print (id2label[p], batch.gold()[i])
-                # print ([(goldt[i][j], tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
-                # print ()
-            if sum(tags[i])!=0:
-                output[-1]['predicted_tags'] = [tags[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-                # print (id2label[p], batch.gold()[i])
-                # print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
-                # print ()
 with open("output_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
     f.write(json.dumps(output))
 p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
