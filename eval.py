b547644f69a0a355e05d5febcb9c339750c7435a
Zheng Tang <oscartang1120@gmail.com>
2021-05-08 20:50:51 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -60,7 +60,7 @@ label2id = constant.LABEL_TO_ID
 id2label = dict([(v,k) for k,v in label2id.items()])
 
 predictions = []
-
+inputs = []
 x = 0
 exact_match = 0
 other = 0
@@ -69,6 +69,9 @@ for c, b in enumerate(batch):
     preds, attn = trainer.predict(b, id2label, tokenizer)
     predictions += preds
     attns += attn
+    batch_size = len(preds)
+    for i in range(batch_size):
+        inputs += [[tokenizer.convert_ids_to_tokens(j) for j in ids[i]]]
 output = list()
 for i, p in enumerate(predictions):
         output.append({'gold_label':batch.gold()[i], 'predicted_label':id2label[p], 'raw_words':batch.words[i], 'predicted_tags':[], 'subj':[], 'obj':[]})
