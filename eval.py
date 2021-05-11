1f9280088adeee24ff413cf82a3be4df357f4de3
Zheng Tang <oscartang1120@gmail.com>
2021-05-10 18:00:07 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -75,7 +75,7 @@ def predict(text):
 
 for i, text in enumerate(batch.words):
     probs = predict(text)
-    exp = explainer.explain_instance(text, predict, num_features=6)
+    exp = explainer.explain_instance(' '.join(text), predict, num_features=6)
     l = label2id[batch.gold()[i]]
     print ('Explanation for class %s' % id2label[l])
     print ('\n'.join(map(str, exp.as_list(label=l))))
