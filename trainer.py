96f85475d74f7cccc11184051a2fbdcf179280a9
Zheng Tang <oscartang1120@gmail.com>
2021-05-10 19:41:18 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -131,4 +131,5 @@ class BERTtrainer(Trainer):
         h = o.pooler_output
         logits = self.classifier(h)
         probs = F.softmax(logits, 1).detach().cpu().numpy()
+        torch.cuda.empty_cache()
         return probs
