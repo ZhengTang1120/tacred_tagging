a60315efd78f6ce4d56389111f0d0a861ce70e10
Zheng Tang <oscartang1120@gmail.com>
2021-05-12 02:34:05 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -48,6 +48,7 @@ model_file = args.model_dir + '/' + args.model
 print("Loading model from {}".format(model_file))
 opt = torch_utils.load_config(model_file)
 opt['device'] = args.device
+opt['batch_size'] = 1
 trainer = BERTtrainer(opt)
 trainer.encoder.model.resize_token_embeddings(len(tokenizer)) 
 trainer.load(model_file)
