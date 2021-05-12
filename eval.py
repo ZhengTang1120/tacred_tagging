e26ac306bd02785a25f1e08cb16a03c080985598
Zheng Tang <oscartang1120@gmail.com>
2021-05-11 22:27:28 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -88,7 +88,9 @@ for i, raw in enumerate(batch.words):
     tagged = eval(tagged)
     # if tagged and batch.gold()[i] != 'no_relation':
     l = label2id[batch.gold()[i]]
-    exp = explainer.explain_instance(text[0], predict, num_features=len(raw), num_samples=2000, labels=[l])
+    pred = np.argmax(probs, axis=1).tolist()
+    predictions += [id2label[pred[0]]]
+    exp = explainer.explain_instance(text[0], predict, num_features=len(raw), num_samples=2000, labels=[l, pred[0]])
     exp.save_to_file('lime_sample%d.html'%i)
 #     lime_token = set([t[0] for t in sorted(exp.as_list(label=l), key=lambda tup: tup[1], reverse=True)[:5]]) - set([w for w in raw if 'SUBJ-' in w or 'OBJ-' in w])
 #     # lime_token = set(list(lime_token)[:len(tagged)])
@@ -98,8 +100,6 @@ for i, raw in enumerate(batch.words):
 #         # r = len(overlap)/len(tagged_token)
 #         # pr = len(overlap)/len(lime_token)
 #         # print (r, ",", pr)
-#     pred = np.argmax(probs, axis=1).tolist()
-#     predictions += [id2label[pred[0]]]
 # output = list()
 # for i, p in enumerate(predictions):
 #     output.append({'gold_label':batch.gold()[i], 'predicted_label':p})
