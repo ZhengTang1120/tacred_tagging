af3e6f2e81e7ed9257c3764c16df2a9345e02ebf
Zheng Tang <oscartang1120@gmail.com>
2021-08-09 16:36:54 -0700

    remove cv

--- eval.py
+++ eval.py
@@ -16,8 +16,6 @@ from transformers import BertTokenizer
 
 import json
 
-import numpy as np
-
 parser = argparse.ArgumentParser()
 parser.add_argument('model_dir', type=str, help='Directory of the model.')
 parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
@@ -44,16 +42,18 @@ special_tokens_dict = {'additional_special_tokens': constant.ENTITY_TOKENS}
 num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
 
 # load opt
-model_file = args.model_dir + '_0/' + args.model
+model_file = args.model_dir + '/' + args.model
 print("Loading model from {}".format(model_file))
 opt = torch_utils.load_config(model_file)
 opt['device'] = args.device
-
+trainer = BERTtrainer(opt)
+trainer.encoder.model.resize_token_embeddings(len(tokenizer)) 
+trainer.load(model_file)
 
 # load data
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 print("Loading data from {} with batch size {}...".format(data_file, opt['batch_size']))
-batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer,  opt['data_dir'] + '/tagging_{}_full.txt'.format(args.dataset), evaluation=True)
+batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer,  opt['data_dir'] + '/tagging_{}.txt'.format(args.dataset), evaluation=True)
 
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
@@ -66,40 +66,35 @@ inputs = []
 subjs = []
 objs = []
 
-chunks = np.array_split(np.array(range(len(batch))),5)
-
-for x, ch in enumerate(chunks):
-    model_file = args.model_dir + '_%d/'%x + args.model
-    trainer = BERTtrainer(opt)
-    trainer.encoder.model.resize_token_embeddings(len(tokenizer)) 
-    trainer.load(model_file)
-    for c, b in enumerate(batch):
-        if c in ch:
-            preds, ts, tagged, ids, s, o = trainer.predict(b, id2label, tokenizer)
-            predictions += preds
-            tags += ts
-            subjs += s
-            objs += o
-            goldt += tagged
-            batch_size = len(preds)
-            for i in range(batch_size):
-                inputs += [[tokenizer.convert_ids_to_tokens(j) for j in ids[i]]]
+x = 0
+exact_match = 0
+other = 0
+for c, b in enumerate(batch):
+    preds, ts, tagged, ids, s, o = trainer.predict(b, id2label, tokenizer)
+    predictions += preds
+    tags += ts
+    subjs += s
+    objs += o
+    goldt += tagged
+    batch_size = len(preds)
+    for i in range(batch_size):
+        inputs += [[tokenizer.convert_ids_to_tokens(j) for j in ids[i]]]
 output = list()
 for i, p in enumerate(predictions):
-    predictions[i] = id2label[p]
-    output.append({'gold_label':batch.gold()[i], 'predicted_label':id2label[p], 'raw_words':batch.words[i], 'predicted_tags':[], 'gold_tags':[], 'subj':[], 'obj':[]})
-    output[-1]['subj'] = [subjs[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-    output[-1]['obj'] = [objs[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-    if sum(goldt[i])!=0:
-        output[-1]['gold_tags'] = [goldt[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-            # print (id2label[p], batch.gold()[i])
-            # print ([(goldt[i][j], tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
-            # print ()
-    if p!=0 and sum(tags[i])!=0:
-        output[-1]['predicted_tags'] = [tags[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
-            # print (id2label[p], batch.gold()[i])
-            # print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
-            # print ()
+        predictions[i] = id2label[p]
+        output.append({'gold_label':batch.gold()[i], 'predicted_label':id2label[p], 'raw_words':batch.words[i], 'predicted_tags':[], 'gold_tags':[], 'subj':[], 'obj':[]})
+        output[-1]['subj'] = [subjs[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
+        output[-1]['obj'] = [objs[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
+        if sum(goldt[i])!=0:
+            output[-1]['gold_tags'] = [goldt[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
+                # print (id2label[p], batch.gold()[i])
+                # print ([(goldt[i][j], tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
+                # print ()
+        if p!=0 and sum(tags[i])!=0:
+            output[-1]['predicted_tags'] = [tags[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
+                # print (id2label[p], batch.gold()[i])
+                # print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
+                # print ()
 # with open("output_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
 #     f.write(json.dumps(output))
 p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
