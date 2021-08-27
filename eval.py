61f05b2056f67dc127fe603336dbe5962559d9c6
Zheng Tang <oscartang1120@gmail.com>
2021-08-27 09:28:41 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -95,8 +95,8 @@ for i, p in enumerate(predictions):
                 # print (id2label[p], batch.gold()[i])
                 # print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
                 # print ()
-# with open("output_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
-#     f.write(json.dumps(output))
+with open("output_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
+    f.write(json.dumps(output))
 p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
 print("{} set evaluate result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,p,r,f1))
 
