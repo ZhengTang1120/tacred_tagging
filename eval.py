c1414c2d98b413cd5f34ae4a489d792d3f19b868
Zheng Tang <oscartang1120@gmail.com>
2021-09-27 21:17:05 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -53,7 +53,7 @@ trainer.load(model_file)
 # load data
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 print("Loading data from {} with batch size {}...".format(data_file, opt['batch_size']))
-batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer,  opt['data_dir'] + '/tagging_dev.txt' evaluation=True)
+batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer,  opt['data_dir'] + '/tagging_dev.txt', evaluation=True)
 
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
