95517aa5de03fb4c44054187072a202a73ff4c6a
Zheng Tang <oscartang1120@gmail.com>
2021-10-26 11:54:14 -0700

    sorted data first

--- dataloader.py
+++ dataloader.py
@@ -26,7 +26,7 @@ class DataLoader(object):
         with open(filename) as infile:
             data = json.load(infile)
         data = self.preprocess(data, opt)
-
+        data = sorted(data, key=lambda f: len(f[0]))
         
         self.id2label = dict([(v,k) for k,v in self.label2id.items()])
         self.labels = [self.id2label[d[-2]] for d in data]
