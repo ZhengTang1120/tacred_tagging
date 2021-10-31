2f4da94b41153d597c4d9fd14bf3c71a920b0041
Zheng Tang <oscartang1120@gmail.com>
2021-10-31 03:03:56 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,170 @@
+import json
+
+CLS = "[CLS]"
+SEP = "[SEP]"
+
+class InputExample(object):
+    """A single training/test example for span pair classification."""
+
+    def __init__(self, guid, sentence, span1, span2, ner1, ner2, label):
+        self.guid = guid
+        self.sentence = sentence
+        self.span1 = span1
+        self.span2 = span2
+        self.ner1 = ner1
+        self.ner2 = ner2
+        self.label = label
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+class DataProcessor(object):
+    """Processor for the TACRED data set."""
+
+    @classmethod
+    def _read_json(cls, input_file):
+        with open(input_file, "r", encoding='utf-8') as reader:
+            data = json.load(reader)
+        return data
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "train.json")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "dev.json")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "test.json")), "test")
+
+    def get_labels(self, data_dir, negative_label="no_relation"):
+        """See base class."""
+        dataset = self._read_json(os.path.join(data_dir, "train.json"))
+        count = Counter()
+        for example in dataset:
+            count[example['relation']] += 1
+        logger.info("%d labels" % len(count))
+        # Make sure the negative label is alwyas 0
+        labels = [negative_label]
+        for label, count in count.most_common():
+            logger.info("%s: %.2f%%" % (label, count * 100.0 / len(dataset)))
+            if label not in labels:
+                labels.append(label)
+        return labels
+
+    def _create_examples(self, dataset, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for example in dataset:
+            sentence = [convert_token(token) for token in example['token']]
+            assert example['subj_start'] >= 0 and example['subj_start'] <= example['subj_end'] \
+                and example['subj_end'] < len(sentence)
+            assert example['obj_start'] >= 0 and example['obj_start'] <= example['obj_end'] \
+                and example['obj_end'] < len(sentence)
+            examples.append(InputExample(guid=example['id'],
+                             sentence=sentence,
+                             span1=(example['subj_start'], example['subj_end']),
+                             span2=(example['obj_start'], example['obj_end']),
+                             ner1=example['subj_type'],
+                             ner2=example['obj_type'],
+                             label=example['relation']))
+        return examples
+
+
+def convert_examples_to_features(examples, label2id, max_seq_length, tokenizer, special_tokens, logger):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    num_tokens = 0
+    num_fit_examples = 0
+    num_shown_examples = 0
+    features = []
+    for (ex_index, example) in enumerate(examples):
+        if ex_index % 10000 == 0:
+            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+
+        tokens = [CLS]
+        SUBJECT_NER = special_tokens["[SUBJ-%s]" % example.ner1]
+        OBJECT_NER = special_tokens["[OBJ-%s]" % example.ner2]
+
+        for i, token in enumerate(example.sentence):
+            if i == example.span1[0]:
+                tokens.append(SUBJECT_NER)
+            if i == example.span2[0]:
+                tokens.append(OBJECT_NER)
+            if (i >= example.span1[0]) and (i <= example.span1[1]):
+                pass
+            elif (i >= example.span2[0]) and (i <= example.span2[1]):
+                pass
+            else:
+                for sub_token in tokenizer.tokenize(token):
+                    tokens.append(sub_token)
+        tokens.append(SEP)
+        num_tokens += len(tokens)
+
+        if len(tokens) > max_seq_length:
+            tokens = tokens[:max_seq_length]
+        else:
+            num_fit_examples += 1
+
+        segment_ids = [0] * len(tokens)
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(input_ids)
+        padding = [0] * (max_seq_length - len(input_ids))
+        input_ids += padding
+        input_mask += padding
+        segment_ids += padding
+        label_id = label2id[example.label]
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+
+        if num_shown_examples < 20:
+            if (ex_index < 5) or (label_id > 0):
+                num_shown_examples += 1
+                logger.info("*** Example ***")
+                logger.info("guid: %s" % (example.guid))
+                logger.info("tokens: %s" % " ".join(
+                        [str(x) for x in tokens]))
+                logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+                logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+                logger.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+                logger.info("label: %s (id = %d)" % (example.label, label_id))
+
+        features.append(
+                InputFeatures(input_ids=input_ids,
+                              input_mask=input_mask,
+                              segment_ids=segment_ids,
+                              label_id=label_id))
+    logger.info("Average #tokens: %.2f" % (num_tokens * 1.0 / len(examples)))
+    logger.info("%d (%.2f %%) examples can fit max_seq_length = %d" % (num_fit_examples,
+                num_fit_examples * 100.0 / len(examples), max_seq_length))
+    return features
+
+
+def convert_token(token):
+    """ Convert PTB tokens to normal tokens """
+    if (token.lower() == '-lrb-'):
+            return '('
+    elif (token.lower() == '-rrb-'):
+        return ')'
+    elif (token.lower() == '-lsb-'):
+        return '['
+    elif (token.lower() == '-rsb-'):
+        return ']'
+    elif (token.lower() == '-lcb-'):
+        return '{'
+    elif (token.lower() == '-rcb-'):
+        return '}'
+    return token
