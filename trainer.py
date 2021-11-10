9c3763064b55b1fd90e4db29757e1296ecb421de
Zheng Tang <oscartang1120@gmail.com>
2021-11-10 08:41:18 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -109,7 +109,7 @@ class BERTtrainer(Trainer):
                     logits = self.classifier(h[i], inputs[0][i].unsqueeze(0), inputs[3][i].unsqueeze(0))
                     loss += self.criterion(logits, labels.unsqueeze(1)[i])
         else:
-            for i, f in enumerate(inputs[3]):
+            for i, f in enumerate(has_tag):
                 if f:
                     loss += self.criterion2(tagging_output[i], inputs[3][i].unsqueeze(1).to(torch.float32))
                     logits = self.classifier(h[i], inputs[0][i].unsqueeze(0), inputs[3][i].unsqueeze(0))
