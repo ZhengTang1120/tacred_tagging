c0504b26a10149f4e786aa46ae649ecddd9b1d64
Zheng Tang <oscartang1120@gmail.com>
2021-11-09 23:36:07 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -41,6 +41,9 @@ class BERTclassifier(nn.Module):
         pool_type = self.opt['pooling']
         h = self.dropout(h)
         out_mask = tags.unsqueeze(2).eq(0) + words.unsqueeze(2).eq(0) + words.unsqueeze(2).ge(20)
+        print (tags)
+        print (words)
+        print (out_mask)
         cls_out = pool(h, out_mask.eq(0), type=pool_type)
         logits = self.classifier(cls_out)
         return logits
