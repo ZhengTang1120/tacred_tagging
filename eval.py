6dee3ffcd6043704e4d3fe23e7e01b9fe6c85066
Zheng Tang <oscartang1120@gmail.com>
2021-11-16 22:14:10 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -74,7 +74,7 @@ for i, p in enumerate(predictions):
         predictions[i] = id2label[p]
         if p!=0:
             print (predictions[i])
-            print (" ".join([t if tags[i][j] == 0 else colored(t, 'red') for j, t in enumerate(batch.words[i]) if j<=128]))
+            print (" ".join([t if j>=len(tags[i]) or tags[i][j] == 0 else colored(t, 'red') for j, t in enumerate(batch.words[i])]))
 
 # with open("output_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
 #     f.write(json.dumps(output))
