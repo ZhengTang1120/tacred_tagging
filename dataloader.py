589d7a492be3385e2e598d54797a96752bfdcbb4
Zheng Tang <oscartang1120@gmail.com>
2021-11-20 02:17:45 -0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -75,10 +75,10 @@ class DataLoader(object):
                     words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[OBJ-'+d['obj_type']+']']+1))
                     tagging_mask.append(0)
                     origin.append((" ".join(d['token'][os:oe+1]), [len(words)]))
-                if i>ss and i<=se:
+                if i>=ss and i<=se:
                     pass
                     # words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[SUBJ-'+d['subj_type']+']']+1))
-                elif i>os and i<=oe:
+                elif i>=os and i<=oe:
                     pass
                     # words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[OBJ-'+d['obj_type']+']']+1))
                 else:
