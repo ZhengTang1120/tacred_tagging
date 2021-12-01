847d854a599d23c237cbfa3bcd6c11e9843e14c6
Zheng Tang <oscartang1120@gmail.com>
2021-11-30 17:00:07 -0700

    Update greedy_adding.py

--- greedy_adding.py
+++ greedy_adding.py
@@ -34,22 +34,21 @@ def preprocess(filename, tokenizer):
         # anonymize tokens
         ss, se = d['subj_start'], d['subj_end']
         os, oe = d['obj_start'], d['obj_end']
-
+        sub_token_len = 0
         for i, t in enumerate(d['token']):
-            if i == ss:
-                words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[SUBJ-'+d['subj_type']+']']+1))
-            if i == os:
-                words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[OBJ-'+d['obj_type']+']']+1))
+            if sub_token_len >= 128:
+                break
+            if i == ss or i == os:
+                sub_token_len += 1
             if i>=ss and i<=se:
-                pass
+                words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[SUBJ-'+d['subj_type']+']']+1))
             elif i>=os and i<=oe:
-                pass
+                words.append("[unused%d]"%(constant.ENTITY_TOKEN_TO_ID['[OBJ-'+d['obj_type']+']']+1))
             else:
                 t = convert_token(t)
-                for j, sub_token in enumerate(tokenizer.tokenize(t)):
-                    words.append(sub_token)
-        words = words[:128]
-        processed.append(words)
+                words.append(t)
+                sub_token_len += len(tokenizer.tokenize(t))
+        processed.append((words, ss, se, os, oe))
     return processed
 
 parser = argparse.ArgumentParser()
@@ -87,6 +86,9 @@ trainer.load(model_file)
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer, True)
 
+with open(opt['data_dir'] + '/tagging_{}.json'.format(args.dataset)) as f:
+    tagging = f.readlines()
+
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
 id2label = dict([(v,k) for k,v in label2id.items()])
@@ -98,20 +100,28 @@ for c, b in enumerate(batch):
     batch_size = len(preds)
 
 data = preprocess(data_file, tokenizer)
-
-for c, words in enumerate(data):
+tagging_scores = list()
+for c, words, ss, se, os, oe in enumerate(data):
+    _, tagged = tagging[c].split('\t')
+    tagged = eval(tagged)
     if predictions[c] != 0:
         l = None
-        rationale = list()
+        rationale = [ss, os]
         while l!=predictions[c]:
             candidates = list()
             cr = list()
             for i in range(len(words)):
-                if i not in rationale:
+                if i not in rationale and i not in range(ss, se+1) and i not in range(os, oe+1):
                     cr.append(i)
                     cand_r = rationale+[i]
                     cand_r.sort()
-                    ids = tokenizer.convert_tokens_to_ids(['[CLS]']+[words[j] for j in cand_r]+['[SEP]'])
+                    tokens = []
+                    for j in cand_r:
+                        if i == se or i == oe:
+                            tokens.append(words[j])
+                        else:
+                            tokens += tokenizer.tokenize(words[j])
+                    ids = tokenizer.convert_tokens_to_ids(['[CLS]']+tokens+['[SEP]'])
                     mask = [1] * len(ids)
                     segment_ids = [0] * len(ids)
                     candidates.append((ids, mask, segment_ids))
@@ -122,6 +132,22 @@ for c, words in enumerate(data):
             rationale.append(cr[b])
         print (id2label[predictions[c]])
         print (" ".join([w if i not in rationale else colored(w, 'red') for i, w in enumerate(words)]))
+        if len(tagged)>0:
+            correct = 0
+            pred = 0
+            for j, t in enumerate(words):
+                if j in rationale and j in tagged:
+                    pred += 1
+                    if j in tagged:
+                        correct += 1
+            r = correct / pred
+            p = correct / len(tagged)
+            f1 = 2.0 * p * r / (p + r)
+            tagging_scores.append((r, p, f1))
+
+tr, tp, tf = zip(*tagging_scores)
+
+print("{} set rationale result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,math.mean(tr),math.mean(tp),math.mean(tf)))
 
 
 
