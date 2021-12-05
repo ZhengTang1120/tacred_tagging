ffdf99d7e4f56465d0355bb420837fcdf8ce063a
Zheng Tang <oscartang1120@gmail.com>
2021-12-05 15:06:17 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -88,7 +88,7 @@ class BERTtrainer(Trainer):
 
         self.optimizer = BertAdam(optimizer_grouped_parameters,
              lr=opt['lr'],
-             warmup=0.5,
+             warmup=opt['warmup_prop'],
              t_total= opt['train_batch'] * opt['burnin'],
              schedule='warmup_constant')
 
