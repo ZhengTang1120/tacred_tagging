8324e5f7d6dd3ebfd5aa7a492d989a300a7b3431
Zheng Tang <oscartang1120@gmail.com>
2021-12-12 21:17:25 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -58,7 +58,7 @@ trainer = BERTtrainer(opt)
 trainer.load(model_file)
 
 # load data
-data_file = opt['data_dir'] + '_{}/{}.json'.format(opt['chunk'], args.dataset)
+data_file = opt['data_dir'] + '/{}_{}.json'.format(opt['chunk'], args.dataset)
 print("Loading data from {} with batch size {}...".format(data_file, opt['batch_size']))
 batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer, True)
 
