560eb6f96169fb9633c15a8c31f2dcf863f8cb02
Zheng Tang <oscartang1120@gmail.com>
2021-12-23 20:24:00 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -169,11 +169,9 @@ class BERTtrainer(Trainer):
         score_max.backward()
 
         saliency, _ = torch.max(embs.grad.data.abs(),dim=2)
-        print (saliency)
         mask = torch.logical_and(inputs[0].gt(0), inputs[0].lt(20))
-        print (mask)
+        mask[0][0] = 0
         saliency = saliency.masked_fill(mask, 0)
-        print (saliency.data.cpu().numpy()[0][1:-1].argsort())
-        top3 = saliency.data.cpu().numpy()[0][1:-1].argsort()[-3:].tolist()
-        print (top3)
+        print (saliency)
+        top3 = saliency.data.cpu().numpy()[0].argsort()[-3:].tolist()
         return predictions, [top3], inputs[0].data.cpu().numpy()[0]
