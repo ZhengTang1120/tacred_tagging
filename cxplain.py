cb5e14366d6515f62da39aacba1937dfee9ea4cd
Zheng Tang <oscartang1120@gmail.com>
2021-12-27 21:13:58 -0700

    Update cxplain.py

--- cxplain.py
+++ cxplain.py
@@ -7,7 +7,7 @@ num_samples = 500
 (x_train, y_train), (x_test, y_test) = TestUtil.get_imdb(word_dictionary_size=num_words,
                                                          num_subsamples=num_samples)
 
-                                                         from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline
 from cxplain.util.count_vectoriser import CountVectoriser
 from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
