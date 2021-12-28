a41fe33dca078284f0576939bc7be7c76c713f1a
Zheng Tang <oscartang1120@gmail.com>
2021-12-28 00:28:19 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -17,6 +17,7 @@ class BERTencoder(nn.Module):
         words = inputs[0]
         mask = inputs[1]
         segment_ids = inputs[2]
+        print (words.size(), mask.size(), segment_ids.size())
         h, pooled_output, embeddings = self.model(words, segment_ids, mask, output_all_encoded_layers=False)
         return pooled_output, embeddings
 
