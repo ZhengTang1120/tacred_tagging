c980cebdb2e192ce98dfefe2f64bc907dae26ef6
Zheng Tang <oscartang1120@gmail.com>
2021-12-28 03:29:32 -0700

    Update cxplain_eval.py

--- cxplain_eval.py
+++ cxplain_eval.py
@@ -129,7 +129,7 @@ class EXModel:
 explained_model = EXModel(trainer)
 
 
-model_builder = RNNModelBuilder(embedding_size=len(tokenizer), with_embedding=True,
+model_builder = RNNModelBuilder(embedding_size=len(tokenizer.vocab), with_embedding=True,
                                 num_layers=2, num_units=32, activation="relu", p_dropout=0.2, verbose=0,
                                 batch_size=32, learning_rate=0.001, num_epochs=2, early_stopping_patience=128)
 masking_operation = WordDropMasking()
