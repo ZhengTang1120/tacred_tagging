b97cf8526271e507e5bb9a53ed51f89e68fd2e92
Zheng Tang <oscartang1120@gmail.com>
2021-12-28 23:54:51 -0700

    Update cxplain_eval.py

--- cxplain_eval.py
+++ cxplain_eval.py
@@ -68,13 +68,14 @@ def preprocess(filename, tokenizer):
                 t = convert_token(t)
                 for j, sub_token in enumerate(tokenizer.tokenize(t)):
                     words.append(sub_token)
+        origin = words[:]
         words = ['[CLS]'] + words + ['[SEP]']
         tokens = tokenizer.convert_tokens_to_ids(words)
         words = [[0] if i>=len(tokens) else [tokens[i]] for i in range(128)]
         output_tokens.append(words)
         relation = [1 if i == constant.LABEL_TO_ID[d['relation']] else 0 for i in range(len(constant.LABEL_TO_ID))]
         labels.append(relation)
-    return np.array(output_tokens).astype(int), np.array(labels).astype(int)
+    return np.array(output_tokens).astype(int), np.array(labels).astype(int), origin
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model_dir', type=str, help='Directory of the model.')
@@ -111,9 +112,9 @@ trainer.load(model_file)
 train_file = opt['data_dir'] + '/train.json'
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 
-x_train, y_train = preprocess(train_file, tokenizer)
-x_test, y_test = preprocess(data_file, tokenizer)
-print (x_train.shape)
+# x_train, y_train, _ = preprocess(train_file, tokenizer)
+x_test, y_test, origin = preprocess(data_file, tokenizer)
+# print (x_train.shape)
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
 id2label = dict([(v,k) for k,v in label2id.items()])
@@ -142,7 +143,7 @@ explained_model = EXModel(trainer)
 
 my_save_directory = "cxplain/"
 # explainer.save(my_save_directory)
-
+output = list()
 explainer = CXPlain.load(my_save_directory)
 attributions = explainer.explain(x_test)
 preds = list()
@@ -152,6 +153,60 @@ for i, t in enumerate(x_test):
     pred = id2label[np.argmax(prob, axis=1).tolist()[0]]
     preds.append(pred)
     golds.append(id2label[np.argmax(y_test[i])])
+    importance = attributions[i]
+    output.append({'gold_label':golds[-1], 'predicted_label':preds[-1], 'predicted_tags':[], 'gold_tags':[]})
+    if preds[-1] != 'no_relation':
+        saliency = []
+        tokens = []
+        c = 0
+        for j, t in enumerate(origin[i]):
+            if j == ss or j == os:
+                c += 1
+            if j>=ss and j<=se:
+                saliency.append(0)
+                tokens.append(colored(t, "blue"))
+            elif j>=os and j<=oe:
+                saliency.append(0)
+                tokens.append(colored(t, "yellow"))
+            else:
+                tokens.append(t)
+                t = convert_token(t)
+                sub_len = len(tokenizer.tokenize(t))
+                saliency.append(importance[c: c+sub_len].mean())
+                c += sub_len
+        top3 = np.array(saliency).argsort()[-3:].tolist()
+        output[-1]["predicted_tags"] = top3
+        tokens = [w if c not in top3 else colored(w, 'red') for c, w in enumerate(tokens)]
+        print (tokens)
+        if len(tagged)>0:
+            output[-1]['gold_tags'] = tagged
+            # print (saliency)
+            # print (output[-1]['gold_label'], output[-1]['predicted_label'])
+            # print (" ".join(tokens))
+            # print (" ".join([w if i not in tagged else colored(w, 'red') for i, w in enumerate(words)]))
+            correct = 0
+            pred = 0
+            for j, t in enumerate(words):
+                if j in top3 and j in tagged:
+                    correct += 1
+            r = correct / 3
+            if len(tagged) > 0:
+                p = correct / len(tagged)
+            else:
+                p = 0
+            try:
+                f1 = 2.0 * p * r / (p + r)
+            except ZeroDivisionError:
+                f1 = 0
+            tagging_scores.append((r, p, f1))
+            print (r, p, f1)
+            print ()
+
+tr, tp, tf = zip(*tagging_scores)
+
+print("{} set rationale result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,statistics.mean(tr),statistics.mean(tp),statistics.mean(tf)))
+with open("output_cxplain_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
+    f.write(json.dumps(output))
 
 p, r, f1 = scorer.score(golds, preds, verbose=True)
 print("{} set evaluate result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,p,r,f1))
