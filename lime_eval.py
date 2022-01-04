1f45c223d7e2accd92dc66155fdffa94dcdadedc
Zheng Tang <oscartang1120@gmail.com>
2022-01-04 12:43:18 -0700

    Update lime_eval.py

--- lime_eval.py
+++ lime_eval.py
@@ -11,6 +11,7 @@ from utils import torch_utils, scorer, constant, helper
 
 from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
 
+from pytorch_pretrained_bert import tokenization
 from transformers import BertTokenizer
 
 import json
