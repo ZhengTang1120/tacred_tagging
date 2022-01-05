428fafe4c3abb8abc7171a3681bd0c4d886a8c3a
Zheng Tang <oscartang1120@gmail.com>
2022-01-04 23:30:24 -0700

    Update eval_rationale.py

--- eval_rationale.py
+++ eval_rationale.py
@@ -20,26 +20,25 @@ for i, item in enumerate(output):
     if predicted_label != "no_relation":
         tagged = item['gold_tags']
         importance = item['predicted_tags']
-        correct = 0
-        pred = 0
-        for j, t in enumerate(words):
-            if j in importance and j in tagged:
-                correct += 1
-        if len(importance) > 0:
-            r = correct / len(importance)
-        else:
-            r = 0
-        if len(tagged) > 0:
-            p = correct / len(tagged)
-        else:
-            p = 0
-        try:
-            f1 = 2.0 * p * r / (p + r)
-        except ZeroDivisionError:
-            f1 = 0
-        tagging_scores.append((r, p, f1))
-        print (r, p, f1)
-        print ()
+        if len(tagged)>0:
+            correct = 0
+            pred = 0
+            for j, t in enumerate(words):
+                if j in importance and j in tagged:
+                    correct += 1
+            if len(importance) > 0:
+                r = correct / len(importance)
+            else:
+                r = 0
+            if len(tagged) > 0:
+                p = correct / len(tagged)
+            else:
+                p = 0
+            try:
+                f1 = 2.0 * p * r / (p + r)
+            except ZeroDivisionError:
+                f1 = 0
+            tagging_scores.append((r, p, f1))
 
 tr, tp, tf = zip(*tagging_scores)
 
