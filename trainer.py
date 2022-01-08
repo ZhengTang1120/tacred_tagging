8296ba5b49f3f8b0a306cbb0d0433bea70670298
Zheng Tang <oscartang1120@gmail.com>
2022-01-08 02:13:47 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -120,8 +120,8 @@ class BERTtrainer(Trainer):
                     if n != -1:
                         logits = self.classifier(h[i], torch.cat(n*[inputs[0][i].unsqueeze(0)], dim=0), tag_cands)
                         best = np.argmax(logits.data.cpu().numpy(), axis=0).tolist()[labels[i]]
-                        # loss += self.criterion2(tagging_output[i], tag_cands[best].unsqueeze(1).to(torch.float32))
-                        loss += self.criterion(logits[best].unsqueeze(0), labels.unsqueeze(1)[i])# + 0.01*torch.sum(tag_cands[best])
+                        loss += self.criterion2(tagging_output[i], tag_cands[best].unsqueeze(1).to(torch.float32))
+                        loss += self.criterion(logits[best].unsqueeze(0), labels.unsqueeze(1)[i]) + 0.01*torch.sum(tag_cands[best])
 
         loss_val = loss.item()
 
