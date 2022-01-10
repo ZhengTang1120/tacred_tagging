29715aeb401e401b3e9bfcd877ba12e9346e9b46
Zheng Tang <oscartang1120@gmail.com>
2022-01-09 22:37:34 -0700

    utils

--- utils/scorer.py
+++ utils/scorer.py
@@ -8,7 +8,7 @@ import argparse
 import sys
 from collections import Counter
 
-NO_RELATION = "Other"
+NO_RELATION = "no_relation"
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Score a prediction file using the gold labels.')
