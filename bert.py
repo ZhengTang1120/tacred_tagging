cdad1e28cbb17171ced2b687c9c26573e1626c2e
Zheng Tang <oscartang1120@gmail.com>
2022-01-10 11:08:06 -0700

    concatenate cls, subj, obj

--- bert.py
+++ bert.py
@@ -18,17 +18,38 @@ class BERTencoder(nn.Module):
         mask = inputs[1]
         segment_ids = inputs[2]
         h, pooled_output = self.model(words, segment_ids, mask, output_all_encoded_layers=False)
-        return pooled_output
+        subj_mask = torch.logical_and(words.unsqueeze(2).gt(4), words.unsqueeze(2).lt(9))# + words.unsqueeze(2).eq(101)
+        obj_mask = torch.logical_and(words.unsqueeze(2).gt(0), words.unsqueeze(2).lt(5))
+        for i, x in enumerate(torch.sum(subj_mask, 1)):
+            if x[0].item() == 0:
+                print (words[i])
+        for i, x in enumerate(torch.sum(obj_mask, 1)):
+            if x[0].item() == 0:
+                print (words[i])
+        return h, pooled_output, subj_mask, obj_mask
 
 class BERTclassifier(nn.Module):
     def __init__(self, opt):
         super().__init__()
         in_dim = 1024
-        self.classifier = nn.Linear(in_dim, opt['num_class'])
+        self.classifier = nn.Linear(3 * in_dim, opt['num_class'])
         self.dropout = nn.Dropout(constant.DROPOUT_PROB)
         self.opt = opt
 
-    def forward(self, h):
-        cls_out = self.dropout(h)#pool(h, out_mask.eq(0), type=pool_type)
+    def forward(self, h, c, subj_mask, obj_mask):
+        cls_out = torch.cat([c, pool(h, subj_mask.eq(0), type="avg"), pool(h, obj_mask.eq(0), type="avg")], 1)
+        cls_out = self.dropout(cls_out)
         logits = self.classifier(cls_out)
         return logits
+
+def pool(h, mask, type='max'):
+    if type == 'max':
+        h = h.masked_fill(mask, -constant.INFINITY_NUMBER)
+        return torch.max(h, 1)[0]
+    elif type == 'avg':
+        h = h.masked_fill(mask, 0)
+        # print ('size: ', (mask.size(1) - mask.float().sum(1)))
+        return h.sum(1) / (mask.size(1) - mask.float().sum(1))
+    else:
+        h = h.masked_fill(mask, 0)
+        return h.sum(1)
--- trainer.py
+++ trainer.py
@@ -93,8 +93,8 @@ class BERTtrainer(Trainer):
         self.encoder.train()
         self.classifier.train()
 
-        h = self.encoder(inputs)
-        logits = self.classifier(h)
+        h, c, mask1, mask2 = self.encoder(inputs)
+        logits = self.classifier(h, c, mask1, mask2)
         loss = self.criterion(logits, labels)
         loss_val = loss.item()
         # backward
@@ -110,8 +110,8 @@ class BERTtrainer(Trainer):
         self.encoder.eval()
         self.classifier.eval()
         with torch.no_grad():
-            h = self.encoder(inputs)
-            probs = self.classifier(h)
+            h, c, mask1, mask2 = self.encoder(inputs)
+            probs = self.classifier(h, c, mask1, mask2)
         loss = self.criterion(probs, labels).item()
         # probs = F.softmax(logits, 1)
         predictions = np.argmax(probs.data.cpu().numpy(), axis=1).tolist()
