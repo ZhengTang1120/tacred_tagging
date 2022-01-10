1aae792b9a5c4272466555623f1eba5af4b93845
Zheng Tang <oscartang1120@gmail.com>
2022-01-10 01:17:17 -0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -84,8 +84,8 @@ class DataLoader(object):
             mask = [1] * len(tokens)
             segment_ids = [0] * len(tokens)
             if self.do_eval:
-                processed += [(tokens, mask, segment_ids, relation, words)]
-            elif (len([aa for aa in tokens if aa>0 and aa<20]) == 2) or relation == 0:
+                processed += [(tokens, mask, segment_ids, ent_mask, relation, words)]
+            elif (len([aa for aa in ent_mask if aa == 1]) == 2) or relation == 0:
                 processed += [(tokens, mask, segment_ids, ent_mask, relation, words)]
         return processed
 
