ff1a892ceebf4c7c802272bd5fac275b20c09be5
Zheng Tang <oscartang1120@gmail.com>
2022-01-10 00:09:21 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -18,7 +18,7 @@ class BERTencoder(nn.Module):
         mask = inputs[1]
         segment_ids = inputs[2]
         h, pooled_output = self.model(words, segment_ids, mask, output_all_encoded_layers=False)
-        return pooled_output
+        return h
 
 class BERTclassifier(nn.Module):
     def __init__(self, opt):
@@ -29,6 +29,20 @@ class BERTclassifier(nn.Module):
         self.opt = opt
 
     def forward(self, h):
-        cls_out = self.dropout(h)#pool(h, out_mask.eq(0), type=pool_type)
+        out_mask = torch.logical_and(words.unsqueeze(2).gt(0), words.unsqueeze(2).lt(7))
+        cls_out = pool(h, out_mask.eq(0), type="avg")
+        cls_out = self.dropout(cls_out)
         logits = self.classifier(cls_out)
         return logits
+
+def pool(h, mask, type='max'):
+    if type == 'max':
+        h = h.masked_fill(mask, -constant.INFINITY_NUMBER)
+        return torch.max(h, 1)[0]
+    elif type == 'avg':
+        h = h.masked_fill(mask, 0)
+        # print ('size: ', (mask.size(1) - mask.float().sum(1)))
+        return h.sum(1) / (mask.size(1) - mask.float().sum(1))
+    else:
+        h = h.masked_fill(mask, 0)
+        return h.sum(1)
