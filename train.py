b0fedcf03d0e744c02863ada051980b4ab193be4
Zheng Tang <oscartang1120@gmail.com>
2022-01-24 13:30:01 -0700

    Update train.py

--- train.py
+++ train.py
@@ -130,7 +130,7 @@ for epoch in range(1, opt['num_epoch']+1):
                 preds, tags, dloss = trainer.predict(batch, id2label, tokenizer)
                 predictions += preds
                 dev_loss += dloss
-            predictions = [id2label[p] for p  ictions]
+            predictions = [id2label[p] for p in predictions]
             train_loss = train_loss / train_num_example * opt['batch_size'] # avg loss per batch
             dev_loss = dev_loss / dev_batch.num_examples * opt['batch_size']
 
