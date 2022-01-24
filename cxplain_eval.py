8eba4029f5ec52508941227dfd86a1383c903c07
Zheng Tang <oscartang1120@gmail.com>
2022-01-24 01:50:26 -0700

    Update cxplain_eval.py

--- cxplain_eval.py
+++ cxplain_eval.py
@@ -174,7 +174,7 @@ for i, t in enumerate(x_test):
                 tokens.append(t)
                 t = convert_token(t)
                 sub_len = len(tokenizer.tokenize(t))
-                saliency.append(importance[c: c+sub_len].mean())
+                saliency.append(float(importance[c: c+sub_len].mean()))
                 c += sub_len
         top3 = np.array(saliency).argsort()[-3:].tolist()
         output[-1]["predicted_tags"] = saliency
