e277b2fb9211feb3b244fec25b2309f02da6e5dd
Zheng Tang <oscartang1120@gmail.com>
2022-01-30 02:08:25 -0700

    Update greedy_adding.py

--- greedy_adding.py
+++ greedy_adding.py
@@ -184,10 +184,10 @@ for c, d in enumerate(data):
 
 
 
-print("{} set rationale result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,statistics.mean(tr),statistics.mean(tp),statistics.mean(tf)))
+
 with open("output_greedy_{}_{}_{}".format(args.model_dir.split('/')[-1], args.dataset, args.model.replace('.pt', '.json')), 'w') as f:
     f.write(json.dumps(output))
 
 tr, tp, tf = zip(*tagging_scores)
-
+print("{} set rationale result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,statistics.mean(tr),statistics.mean(tp),statistics.mean(tf)))
 
