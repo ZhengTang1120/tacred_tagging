6cabc1f9977b71130f6a1818548a9d3b8068c047
Zheng Tang <oscartang1120@gmail.com>
2022-01-30 01:46:00 -0700

    Update greedy_adding.py

--- greedy_adding.py
+++ greedy_adding.py
@@ -89,14 +89,15 @@ model_file = args.model_dir + '/' + args.model
 print("Loading model from {}".format(model_file))
 opt = torch_utils.load_config(model_file)
 opt['device'] = args.device
+opt['do_rationale'] = False
+opt['rationale'] = False
 trainer = BERTtrainer(opt)
 trainer.load(model_file)
 
 # load data
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 batch = DataLoader(data_file, opt['batch_size'], opt, tokenizer, True)
-opt['do_rationale'] = False
-opt['rationale'] = False
+
 with open(opt['data_dir'] + '/tagging_{}.txt'.format(args.dataset)) as f:
     tagging = f.readlines()
 
