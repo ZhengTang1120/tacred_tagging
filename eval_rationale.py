0470459a231eaaf427afe848afe23da5704fcd7b
Zheng Tang <oscartang1120@gmail.com>
2022-01-30 03:08:27 -0700

    Update eval_rationale.py

--- eval_rationale.py
+++ eval_rationale.py
@@ -77,7 +77,7 @@ for i, item in enumerate(output):
                 # elif w in importance:
                 #     tokens.append('<span style="color:red;">%s</span>'%word)
                 else:
-                    col = cm(item['predicted_tags'][w]*100+25)
+                    col = rgb2hex(cm(item['predicted_tags'][w]*100+25))
                     tokens.append('<span style="color:%s;">%s</span>'%(col, word))
         else:
             for w, word in enumerate(words):
