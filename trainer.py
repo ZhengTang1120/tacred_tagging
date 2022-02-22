06d508ff430eb57d08abdebd8c04bee1369cdc4f
Zheng Tang <oscartang1120@gmail.com>
2022-02-21 23:41:37 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -136,8 +136,10 @@ class BERTtrainer(Trainer):
         with torch.no_grad():
             h, b_out = self.encoder(inputs)
             tagging_output = self.tagger(h)
+            words = inputs[0]
+            ent_mask = torch.logical_and(words.unsqueeze(2).ge(0), words.unsqueeze(2).lt(9))
             tagging_mask = torch.round(tagging_output).squeeze(2).eq(0)
-            tagging_max = np.argmax(tagging_output.squeeze(2).data.cpu().numpy(), axis=1)
+            tagging_max = np.argmax(tagging_output.masked_fill(ent_mask, -constant.INFINITY_NUMBER).squeeze(2).data.cpu().numpy(), axis=1)
             tagging = torch.round(tagging_output).squeeze(2)
             logits = self.classifier(h, inputs[0], tagging_mask)
             probs = F.softmax(logits, 1) * torch.round(b_out)
