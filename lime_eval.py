235b819289b4f81c98e040dc2dd19f28dfb556c8
Zheng Tang <oscartang1120@gmail.com>
2022-03-02 01:13:27 -0700

    false bug on lime...

--- lime_eval.py
+++ lime_eval.py
@@ -116,7 +116,7 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 def predict(texts):
-    texts = [['[CLS]'] +[x if x!='' else '[MASK]' for x in t.split(' ')]+ ['[SEP]'] for t in texts]
+    texts = [[x if x!='' else '[MASK]' for x in t.split(' ')] for t in texts]
     tokens = [tokenizer.convert_tokens_to_ids(t) for t in texts]
     probs = None
     for batch in chunks(tokens, 32):
