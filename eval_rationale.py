16fe665d3a87e09a99ea75c407d5ced90b9b710d
Zheng Tang <oscartang1120@gmail.com>
2022-03-28 02:11:48 -0700

    Update eval_rationale.py

--- eval_rationale.py
+++ eval_rationale.py
@@ -128,7 +128,7 @@ for i, item in enumerate(output):
         # elif i in [2772, 792, 2111, 3396, 2029, 865, 1236, 3498, 2897, 425, 1882, 1598, 3707, 579, 16, 3215, 1876, 3616, 813, 2691, 1226, 3761, 388, 2391, 1734, 1640, 2011, 600, 3318, 2379]:
         #     writer.writerow({'relation': relation, 'text': text, 'subj_type':origin[i]['subj_type'], 'obj_type':origin[i]['obj_type'], 'subj':" ".join(subj), 'obj':" ".join(obj), "gold": gold, "source":args.data})
 
-        if len(tagged)>0:
+        if len(tagged)>0 and gold_label == predicted_label:
             correct = 0
             pred = 0
             for j, t in enumerate(words):
