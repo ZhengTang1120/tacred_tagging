da24d635f774a0e482acd7195e8a34919a89dbab
Zheng Tang <oscartang1120@gmail.com>
2022-05-16 13:20:23 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -53,11 +53,10 @@ def unpack_batch(batch, cuda, device, num_class):
     if cuda:
         with torch.cuda.device(device):
             inputs = [batch[i].to('cuda') for i in range(3)]
-            print (batch[-1].float().cuda())
-            labels = Variable(F.one_hot(batch[-1].cuda(), num_classes=num_class))
+            labels = Variable(F.one_hot(batch[-1].cuda(), num_classes=num_class)).float()
     else:
         inputs = [Variable(batch[i]) for i in range(3)]
-        labels = Variable(F.one_hot(batch[-1], num_classes=num_class))
+        labels = Variable(F.one_hot(batch[-1], num_classes=num_class)).float()
     return inputs, labels
 
 class BERTtrainer(Trainer):
