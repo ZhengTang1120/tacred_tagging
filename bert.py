e9e744dde29bf18e1b67a1dfa4970ac95c9585bd
Zheng Tang <oscartang1120@gmail.com>
2022-05-16 12:22:21 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -44,7 +44,7 @@ def pool(h, mask, type):
     elif type == 'avg':
         h = h.masked_fill(mask, 0)
         # print ('size: ', (mask.size(1) - mask.float().sum(1)))
-        return h.sum(1) / (mask.size(1) - mask.float().sum(1))
+        return h.sum(1) / (mask.size(1) - mask.float().sum(1)) if mask.size(1) > mask.float().sum(1) else h.sum(1) # solution for masked all tokens, not sure if there is a better solution or not...
     else:
         h = h.masked_fill(mask, 0)
         return h.sum(1)
