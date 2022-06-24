42ddcc4a56c1e39e790ab14cc606019055659135
Zheng Tang <oscartang1120@gmail.com>
2022-06-24 14:14:38 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -33,6 +33,7 @@ class BERTclassifier(nn.Module):
         h2 = torch.cat([pool(h, subj_mask.eq(0), type="avg"), pool(h, obj_mask.eq(0), type="avg")], 1)
         rationale = torch.sigmoid(self.generator(F.relu(self.dropout(h2))))
         rationale_mask = torch.round(rationale)
+        print (h2.size(), h.size(), rationale_mask.size())
         cls_out = torch.cat([pool(h, rationale_mask.eq(0), type="avg"), h2], 1)
         cls_out = self.dropout(cls_out)
         logits = self.classifier(cls_out)
