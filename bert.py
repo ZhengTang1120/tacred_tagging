fe4a82cd4116131742612626c8b82ccd5df4c3bb
Zheng Tang <oscartang1120@gmail.com>
2022-07-14 22:03:03 -0700

    change back

--- bert.py
+++ bert.py
@@ -9,30 +9,41 @@ from pytorch_pretrained_bert.modeling import BertModel
 from utils import constant, torch_utils
 
 class BERTencoder(nn.Module):
-    def __init__(self, num_class):
+    def __init__(self):
         super().__init__()
-        in_dim = 1024 
+        in_dim = 1024
         self.model = BertModel.from_pretrained("spanbert-large-cased")
-        self.classifier = nn.Linear(in_dim, num_class)
         self.dropout = nn.Dropout(constant.DROPOUT_PROB)
 
-    def forward(self, inputs, rationale_mask):
+    def forward(self, inputs):
         words = inputs[0]
         mask = inputs[1]
         segment_ids = inputs[2]
+        h, pooled_output = self.model(words, segment_ids, mask, output_all_encoded_layers=False)
+        
+        return h
 
+class BERTclassifier(nn.Module):
+    def __init__(self, opt):
+        super().__init__()
+        in_dim = 1024
+        self.classifier = nn.Linear(3 * in_dim, opt['num_class'])
+        self.dropout = nn.Dropout(constant.DROPOUT_PROB)
+        self.generator = nn.Linear(in_dim, 1)
+        self.opt = opt
+
+    def forward(self, h, words):
+        pool_type = self.opt['pooling']
         subj_mask = torch.logical_and(words.unsqueeze(2).gt(0), words.unsqueeze(2).lt(3))
         obj_mask = torch.logical_and(words.unsqueeze(2).gt(2), words.unsqueeze(2).lt(20))
-        entity_mask = subj_mask + obj_mask
-        rationale_mask.masked_fill(entity_mask.squeeze(2), 1) # Force to set subject and object as important features
-
-        h, pooled_output = self.model(words, segment_ids, rationale_mask, output_all_encoded_layers=False)
-        # cls_out = torch.cat([pool(h, subj_mask.eq(0), type="avg"), pool(h, obj_mask.eq(0), type="avg")], 1)
-        cls_out = self.dropout(pooled_output)
+        rationale = torch.sigmoid(self.generator(F.relu(self.dropout(h))))
+        rationale_mask = torch.round(rationale)
+        cls_out = torch.cat([pool(h, rationale_mask.eq(0), type=pool_type), pool(h, subj_mask.eq(0), type=pool_type), pool(h, obj_mask.eq(0), type=pool_type)], 1)
+        cls_out = self.dropout(cls_out)
         logits = self.classifier(cls_out)
-        return logits
+        return logits, rationale_mask
 
-def pool(h, mask, type):
+def pool(h, mask, type='max'):
     if type == 'max':
         h = h.masked_fill(mask, -constant.INFINITY_NUMBER)
         return torch.max(h, 1)[0]
